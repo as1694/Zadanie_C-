590a5a9836077f8d77f8f30a888b63640e234977
as1694 <32881486+as1694@users.noreply.github.com>
2018-03-12 23:50:04 +0100

    first commit

---
+++ Zadanie_konsola/Data.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie_konsola
+{
+    public class Data
+    {
+        public String Name
+        {
+            get;
+            private set;
+        }
+
+        public String Surname
+        {
+            get;
+            private set;
+        }
+
+        public String Adress
+        {
+            get;
+            private set;
+        }
+
+        public String Phone
+        {
+            get;
+            private set;
+        }
+
+        public Data()
+        {
+            Name = null;
+            Surname = null;
+            Adress = null;
+            Phone = null;
+        }
+
+        public void Set(byte what, String value)
+        {
+            switch (what)
+            {
+                case 1: Name = value; break;
+                case 2: Surname = value; break;
+                case 3: Adress = value; break;
+                case 4: Phone = value; break;
+                default: Console.WriteLine("Błąd"); break;
+            }
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Imię: " + Name);
+            Console.WriteLine("Nazwisko: " + Surname);
+            Console.WriteLine("Adres: " + Adress);
+            Console.WriteLine("Telefon: " + Phone);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Zadanie_konsola/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie_konsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data();
+
+            byte counter = 1;
+            char decission = 's';
+            String value = null;
+            String what = null;
+
+            while(counter<5)
+            {
+                switch(counter)
+                {
+                    case 1:
+                        {
+                            what = "imie";
+                            if (data.Name == null || decission == 's' || decission == 'S')
+                            {
+                                data.Write();
+                                Console.WriteLine("Podaj imię:");
+                                value = Console.ReadLine();
+                                data.Set(counter, value);
+                            }
+                        } break;
+                    case 2:
+                        {
+                            what = "nazwisko";
+                            if (data.Surname == null || decission == 's' || decission == 'S')
+                            {
+                                data.Write();
+                                Console.WriteLine("Podaj nazwisko:");
+                                value = Console.ReadLine();
+                                data.Set(counter, value);
+                            }
+                        } break;
+                    case 3:
+                        {
+                            what = "adres";
+                            if (data.Adress == null || decission == 's' || decission == 'S')
+                            {
+                                data.Write();
+                                Console.WriteLine("Podaj adres:");
+                                value = Console.ReadLine();
+                                data.Set(counter, value);
+                            }
+                        } break;
+                    case 4:
+                        {
+                            what = "telefon";
+                            if (data.Phone == null || decission == 's' || decission == 'S')
+                            {
+                                data.Write();
+                                Console.WriteLine("Podaj telefon:");
+                                value = Console.ReadLine();
+                                data.Set(counter, value);
+                            }
+                        } break;
+
+                    default:
+                        Console.WriteLine("błąd");
+                        break;
+                }
+                Console.Clear();
+                data.Write();
+
+                if (counter==4)
+                    Console.WriteLine("Koniec (N)/ Wstecz (B)/ Zmień "+what+" (S)");
+                else if (counter>1)
+                    Console.WriteLine("Dalej (N)/ Wstecz (B)/ Zmień " + what + " (S)");
+                else
+                    Console.WriteLine("Dalej (N)/ Zmień " + what + " (S)");
+
+                decission = Console.ReadKey().KeyChar;
+                if(decission == 's' || decission == 'S')
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else if (counter > 1 && decission == 'b' || decission == 'B')
+                    counter--;
+                else if (decission == 'n' || decission == 'N')
+                    counter++;
+               
+
+                Console.Clear();
+            }
+            Console.WriteLine("Twoje dane:");
+            Console.WriteLine();
+            data.Write();
+            Console.Read();
+        }
+    }
+}
